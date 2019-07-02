f4ae1c2e721ff546630903e4ee78298a18d8aa09
Brad Just <bradpjust@gmail.com>
2019-07-02 15:29:39 -0500

    Update __init__.py

--- justMFGApp/__init__.py
+++ justMFGApp/__init__.py
@@ -9,6 +9,6 @@ app.secret_key = 'development key'
 Data = WorkOrders()
 
 #Extract data from JSON file
-Data.setData('/Users/bradjust/desktop/projects/Just MFG Projects/WorkOrderProj_Python/justMFGApp/JSON/WorkOrderView1.json') #input a valid path to a json file to be parsed
+Data.setData('') #input a valid path to a json file to be parsed
 
 from justMFGApp import routes
