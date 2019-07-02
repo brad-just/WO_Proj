5a6c770f852b66492148675d0c926d0c9d4ada0c
brad-just <bradpjust@gmail.com>
2019-07-02 14:10:05 -0500

    Forms

--- justMFGApp/__init__.py
+++ justMFGApp/__init__.py
@@ -3,12 +3,12 @@ from justMFGApp.WorkOrders import WorkOrders
 
 #Register the app
 app = Flask(__name__)
+app.secret_key = 'development key'
 
 #Create a new WorkOrders object called data
 Data = WorkOrders()
 
 #Extract data from JSON file
-Data.setData('PATH_TO_JSON') #input a valid path to a json file to be parsed
-Data.setDepartment("DEPARTMENT") #input the department to be displayed
+Data.setData('/Users/bradjust/desktop/projects/Just MFG Projects/WorkOrderProj_Python/justMFGApp/JSON/WorkOrderView1.json') #input a valid path to a json file to be parsed
 
 from justMFGApp import routes
--- justMFGApp/routes.py
+++ justMFGApp/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect, session
 from justMFGApp import app, Data
+from justMFGApp.forms import DepartmentForm
 from operator import itemgetter
 from datetime import datetime
 
@@ -14,20 +15,44 @@ def strToZero(dic, t):
 @app.route("/", methods=['POST', 'GET'])
 @app.route("/index", methods=['POST', 'GET'])
 def index():
-    if(request.method=='POST'): #The user wants to sort the data on a specific key. The sort is done here
-        if(Data.descending):
-            Data.descending = False #Toggles accending sort and descending sort with the same button
-        else:
-            Data.descending = True
-        for title in Data.header:
-            if(title in request.form):
-                if(title == 'Sales Order'):
-                    Data.sort(criteria=lambda i: (strToZero(i,title), i[title])) #Because 'STOCK' appears in this field, we need to convert everything to a string before sorting
-                else:
-                    Data.sort(criteria=itemgetter(title)) #Sort dictionary by the title field
-    return(render_template("index.html", orders=Data, datetime=datetime))
+    if(session.get('logged_in')):
+        Data.setDepartment(session['department'])
+        if(request.method=='POST'): #The user wants to sort the data on a specific key. The sort is done here
+            if(Data.descending):
+                Data.descending = False #Toggles accending sort and descending sort with the same button
+            else:
+                Data.descending = True
+            for title in Data.header:
+                if(title in request.form):
+                    if(title == 'Sales Order'):
+                        Data.sort(criteria=lambda i: (strToZero(i,title), i[title])) #Because 'STOCK' appears in this field, we need to convert everything to a string before sorting
+                    else:
+                        Data.sort(criteria=itemgetter(title)) #Sort dictionary by the title field
+        return(render_template("index.html", orders=Data, datetime=datetime))
+    else:
+        return(redirect(url_for('login')))
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if(session.get('logged_in')):
+        Data.setDepartment(session['department']) #the session variable holds the cookies stored by the app
+        return(redirect(url_for('index')))
+    else:
+        form = DepartmentForm()
+        if(request.method=='POST'):
+            if(request.form['department'] != ''):
+                session['logged_in'] = True
+                session['department'] = request.form['department'].title()
+                Data.setDepartment(request.form['department'].title())
+                return(redirect(url_for('index')))
+    return(render_template("login.html", orders=Data, form=form))
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    session['department'] = ''
+    Data.setDepartment('')
+    return(redirect(url_for('login')))
 
 
-@app.route("/pdf")
-def pdf():
-    return(render_template("pdf.html", orders=Data))
+    
