25bdb635801fa8ea7bea19bb8fa9329fcec7d3bc
brad-just <bradpjust@gmail.com>
2019-07-11 13:31:54 -0500

    JSON Converter

--- justMFGApp/__init__.py
+++ justMFGApp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from justMFGApp.WorkOrders import WorkOrders
+from justMFGApp.translateJSON import translateJSON
 
 #Register the app
 app = Flask(__name__)
@@ -9,7 +10,17 @@ app.config['PRODUCTION_PDFS'] = "//SQL-ERP/ProjectEngineering/Production Drawing
 #Create a new WorkOrders object called data
 Data = WorkOrders()
 
+#Create new JSON
+readPath = '/Users/bradjust/desktop/projects/Just MFG Projects/WorkOrderProj_Python/justMFGApp/JSON/MattG_TestFileforJSON.txt'
+writePath = '/Users/bradjust/desktop/projects/Just MFG Projects/WorkOrderProj_Python/justMFGApp/JSON/MattG_TestFileforJSON.json'
+
+header = ['WorkOrderNo', 'SoftWONo', 'TranNoRel', 'LineNo', 'QtyOrd', 'LineStatus', 'TranDate', 'PromiseDate', \
+          'ItemID', 'Short Description','Hold', 'HoldReason','CustName', 'PrintedExpectShipRpt', 'QtyOnBO',\
+          'QtyShip', 'QtyInvcd', 'QtyOpenToShip', 'OrderHold', 'OrderHoldReason']
+
+translateJSON(readPath, writePath, head=header, sep=';')
+
 #Extract data from JSON file
-Data.setData('/home/bradjust/WorkOrderApp/WO_Proj/justMFGApp/JSON/WorkOrderView1.json') #input a valid path to a json file to be parsed
+Data.setData(writePath) #input a valid path to a json file to be parsed
 
 from justMFGApp import routes
