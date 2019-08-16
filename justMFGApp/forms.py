cee45ce49ccba17670229ac594e50c5dfd7418ed
Brad Just <bradpjust@gmail.com>
2019-08-16 17:12:14 -0500

    Delete forms.py

--- justMFGApp/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
-
-class DepartmentForm(FlaskForm):
-   department = SelectField('Select Department', choices = [('custom', 'Custom'), ('bowl', 'Bowl')])
-   submit = SubmitField("Go")
